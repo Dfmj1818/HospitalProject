3f634ed7c3a44c6bd72f9b37a1ddf34e5c260d1e
Dfmj1818 <danieljimenezgg@gmail.com>
2023-09-26 20:56:16 -0500

    Improvement in methods to filter by hour

--- src/Model/MedicalAppoinmentManager.java
+++ src/Model/MedicalAppoinmentManager.java
@@ -1,44 +1,110 @@
 package Model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import Exceptions.EmptyListException;
+
 public class MedicalAppoinmentManager  {
-	private List<MedicalAppoinment>medicalAppoinments;
+	private List<MedicalAppoinment>medicalAppoinmentsList;
 
 	public MedicalAppoinmentManager() {
-		medicalAppoinments=new ArrayList<MedicalAppoinment>();
+		medicalAppoinmentsList=new ArrayList<MedicalAppoinment>();
 	}
 
 	public void setAvaiablesMedicalAppoinmentsList(List<MedicalAppoinment>avaiablesmedicalAppoinmentList){
-		this.medicalAppoinments=avaiablesmedicalAppoinmentList;
+		this.medicalAppoinmentsList=avaiablesmedicalAppoinmentList;
 	}
 
 	public List<MedicalAppoinment>getAvaiablestMedicalAppoinmentsList(){
-		return medicalAppoinments;
+		return medicalAppoinmentsList;
 	}
-	
+
 	public void setMedicalAppoinmentId() {
-		AtomicInteger id=new AtomicInteger(0);
-		medicalAppoinments.stream().forEach(medicalAppoinment->medicalAppoinment.setId(id.getAndIncrement()));
+		int id=0;
+		for(MedicalAppoinment medicalAppoinment:medicalAppoinmentsList){
+			id++;
+			medicalAppoinment.setId(id);
+		}
 	}
-	
+
 	public List<MedicalAppoinment> getAvaiablesMedicalAppoinment() {
-		List<MedicalAppoinment>avaiablesMedicalAppoinments=medicalAppoinments.stream()
+		List<MedicalAppoinment>avaiablesMedicalAppoinments=medicalAppoinmentsList.stream()
 				.filter(medicalAppoinment->medicalAppoinment.getStateOfMedicalAppoinment())
 				.collect(Collectors.toList());
 
 		return avaiablesMedicalAppoinments;
 	}
-	
-	
+
+	public void medicalAppoinmentsCreationAndSetHour(){
+		LocalDateTime startTime=LocalDateTime.parse("2023-09-25T08:00:00");	
+		LocalDateTime lastDayOfTheYear=LocalDateTime.parse("2023-12-31T18:00");
+		int hoursPassedInTheDay=0;
+
+		while(startTime.isBefore(lastDayOfTheYear)){
+			MedicalAppoinment medicalAppoinment=new MedicalAppoinment();
+			medicalAppoinment.setDateOfAppoinment(startTime.plusHours(hoursPassedInTheDay));
+			medicalAppoinmentsList.add(medicalAppoinment);
+			hoursPassedInTheDay++;
+
+			if(hoursPassedInTheDay>=10){
+				hoursPassedInTheDay=0;
+				startTime=startTime.plusDays(1);
+			}			
+		}
+
+	}
+
+	public void checkIfTheListIsEmpty() {
+		if(medicalAppoinmentsList.isEmpty()){
+			throw new EmptyListException();
+		}
+	}
+
+	public void assignDoctors(){
+		for(MedicalAppoinment medicalAppoinment:medicalAppoinmentsList){
+            //pendiente implementar
+		}
+	}
+
+
+	public List<MedicalAppoinment>filterByAppoinmentType(int selectedMedicalAppoinment){
+		List<MedicalAppoinment>filteredListByAppoinmentType=medicalAppoinmentsList.stream()
+				.filter(medicalAppoinment->medicalAppoinment.getAppoinmentType()==selectedMedicalAppoinment)
+				.collect(Collectors.toList());
+
+		return filteredListByAppoinmentType;
+	}
+
+	public List<MedicalAppoinment>filterMedicalAppoinmentsByMorningHour(){
+		List<MedicalAppoinment>listFilteredByMorningHour= medicalAppoinmentsList.stream()
+				.filter(medicalAppoinment->{
+					int medicalAppoinmentHour=medicalAppoinment.getDateOfAppoinment().getHour();
+					return medicalAppoinmentHour>=8&&medicalAppoinmentHour<=12;
+				})
+				.collect(Collectors.toList());
+
+		return listFilteredByMorningHour;           
+	}
+
+	public List<MedicalAppoinment>filterMedicalAppoinmentsByAfternoonHour(){
+		List<MedicalAppoinment>listFilteredByAfternoonHour=medicalAppoinmentsList.stream()
+				.filter(medicalAppoinment->{
+					int medicalAppoinmentHour=medicalAppoinment.getDateOfAppoinment().getHour();
+					return medicalAppoinmentHour>=13;
+				})
+				.collect(Collectors.toList());
+
+		return listFilteredByAfternoonHour;
+	}
+
 	public void modifyStateOfMedicalAppoinment(MedicalAppoinment medicalApppoinment){
 		medicalApppoinment.setStateOfMedicalAppoinment(false);
 	}
-	 
-
 
 
 }
